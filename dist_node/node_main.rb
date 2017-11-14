a0d46e8ef692e0053fac6716b12da17969bae953
seseri <00ing.n@gmail.com>
2017-11-14 17:45:05 +0900

    move some functions to node_manager

--- dist_node/node_main.rb
+++ dist_node/node_main.rb
@@ -1,20 +1,28 @@
 require 'socket'
+#require 'node_manager'
+require File.dirname(__FILE__) + "/node_manager"
 
 #get host's ip adress from command line
 #(in order to get ip informations about other nodes)
 host_ip = ARGV[0]
 #Port number is fixed
 #In future, port number must be shared in project using some procedure
-port = 49153
-server = TCPServer.open(port)
 
-hash={}
+#port = 49153
+#server = TCPServer.open(port)
+
+#hash={}
+manager = NodeManager.new()
 
 #Move as hash on network
 while true
+  manager.thread
+=begin
   client = server.accept
   s = client.gets.split
-  
+
+  manager.allocate(s)
+ 
   #put([1], [2])
   if s[0]=="put" then
     hash[s[1]] = s[2]
@@ -29,6 +37,7 @@ while true
   p hash
 
   client.close
+=end
 end
 
-server.close
+#server.close
--- dist_node/node_manager.rb
+++ dist_node/node_manager.rb
@@ -2,16 +2,53 @@ require 'socket'
 require 'httpclient'
 require 'json'
 
+$port = 49153
+
 class NodeManager
-  port = 49153
-  @@iplist
-    
+  
+  def initialize()
+    #server.close is may not needed because it will be closed
+    #when the class instance be garvage collected.
+    #(is it really true????)
+    @server = TCPServer.open($port)
+
+    @hash={}
+    @iplist={}    
+  end
+
+  def thread
+    @client = @server.accept
+
+    allocate(@client.gets.split)
+
+    @client.close
+  end
+
+
+  def allocate(s)
+      #put([1], [2])
+    if s[0]=="put" then
+      @hash[s[1]] = s[2]
+    end
+ 
+    #get([1])  
+    if s[0]=="get" then
+      p @hash[s[1]]
+      @client.write(@hash[s[1]])
+    end
+
+  p @hash
+  end
+
+  #run when iplist is updated (triggerd by request from leader server)
+  #get new iplist , reculclate and remap the hash map
+  def update
+  end
+
   #Get(when startup) or update iplist from iphost
   def get_iplist(host_ip)
     http_client = HTTPClient.new(host_ip)
-    iplist = JSON.parse(http_client.get())
+    @iplist = JSON.parse(http_client.get())
   end
-
-  
   
 end
